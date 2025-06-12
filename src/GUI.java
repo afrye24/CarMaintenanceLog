28e74bdb270381bd6552d473d3ee5da910c4aacd
afrye24 <aniyaf24@vt.edu>
2025-06-11 22:50:58 -0400

    More code

--- src/GUI.java
+++ src/GUI.java
@@ -4,43 +4,41 @@ import java.awt.*;
 import java.awt.event.*;
 import java.text.NumberFormat;
 
-public class GUI
-{
-    private JPanel panel;
-    private JFrame frame;
-    private String ford;
-    private String honda;
-    private String chevrolet;
-    private String hyundai;
-    private String toyota;
-    private JLabel selectMake;
+public class GUI {
+    private final JPanel panel;
+    private final JFrame frame;
+    private String selection;
+    private JLabel labelMileage;
+    private JFormattedTextField mileage;
+    private JButton enterButton;
+    private JComboBox fordList;
+    private JComboBox hondaList;
+    private JComboBox toyotaList;
+    private JComboBox hyundaiList;
+    private JComboBox chevroletList;
+    private JLabel labelModel;
 
     GUI() {
+
         frame = new JFrame(" Car Maintenance Tool");
-        panel = new JPanel(new GridLayout(5,4,5,5));
+        panel = new JPanel(new GridLayout(5, 4, 5, 5));
         panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 10, 20));
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
-        ford = "Ford";
-        honda = "Honda";
-        chevrolet = "Chevrolet";
-        toyota = "Toyota";
-        hyundai = "Hyundai";
+        frame.setSize(500, 500);
         listOfYears();
         listOfMakes();
-        modelLabel();
-        textboxForMileage();
         frame.setVisible(true);
         frame.pack();
     }
-    public void listOfYears()
-    {
+
+    public void listOfYears() {
         JLabel labelYear = new JLabel("Vehicle Year");
         labelYear.setFont(new Font("Arial", Font.PLAIN, 15));
         labelYear.setHorizontalAlignment(2);
         panel.add(labelYear);
         String[] carYears = {
+                "Select Year",
                 "2010",
                 "2011",
                 "2012",
@@ -60,19 +58,15 @@ public class GUI
 
             }
         });
-
-
         panel.add(yearsList);
-
-
     }
 
-    public void listOfMakes()
-    {
+    public void listOfMakes() {
         JLabel labelMake = new JLabel("Vehicle Make");
         labelMake.setFont(new Font("Arial", Font.PLAIN, 15));
         panel.add(labelMake);
         String[] carMakes = {
+                "Select Make",
                 "Ford",
                 "Chevrolet",
                 "Toyota",
@@ -81,161 +75,196 @@ public class GUI
         };
         JComboBox makeList = new JComboBox(carMakes);
         panel.add(makeList);
+
         makeList.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
+            public void actionPerformed(ActionEvent e) {
+                selection = makeList.getSelectedItem().toString();
+                selectionDeletion();
+                selectionAddition();
+                panel.revalidate();
+                frame.pack();
+            }
+        });
+
+    }
+
+    public void selectionAddition() {
+        modelLabel();
+        if (selection.equals("Ford")) {
+            String[] fordModel = {
+                    "Explorer",
+                    "Edge",
+                    "Maverick",
+                    "Transit",
+                    "F-Series",
+                    "Bronco",
+                    "Mustang",
+                    "Expedition",
+                    "Bronco Sport"
+            };
+            fordList = new JComboBox(fordModel);
+            panel.add(fordList);
+            textboxForMileage();
+        }
+        if (selection.equals("Honda")) {
+            String[] hondaModel = {
+                    "CR-V",
+                    "Civic",
+                    "Accord",
+                    "Pilot",
+                    "HR-V",
+                    "Odyssey",
+                    "Prologue",
+                    "Ridgeline",
+                    "Passport"
+            };
+            hondaList = new JComboBox(hondaModel);
+            panel.add(hondaList);
+            textboxForMileage();
+
+        }
+        if (selection.equals("Hyundai")) {
+            String[] hyundaiModel = {
+                    "Tucson",
+                    "Elantra",
+                    "Palisade",
+                    "Santa Fe",
+                    "Kona",
+                    "Ioniq-5",
+                    "Sonata",
+                    "Santa Cruz",
+                    "Venue"
+            };
+            hyundaiList = new JComboBox(hyundaiModel);
+            panel.add(hyundaiList);
+            textboxForMileage();
+
+        }
+        if (selection.equals("Toyota")) {
+            String[] toyotaModel = {
+                    "Camry",
+                    "Corolla",
+                    "RAV4",
+                    "Highlander",
+                    "Tacoma",
+                    "Tundra",
+                    "Sienna",
+                    "4Runner",
+                    "Prius"
+            };
+            toyotaList = new JComboBox(toyotaModel);
+            panel.add(toyotaList);
+            textboxForMileage();
+
+        }
+        if (selection.equals("Chevrolet")) {
             {
-                String selection = makeList.getSelectedItem().toString();
-                if(selection.equals(ford))
-                {
-                    fordModels();
-                }
-                else if(selection.equals(honda))
-                {
-                    hondaModels();
-                }
-                else if(selection.equals(hyundai))
-                {
-                    hyundaiModels();
-                }
-                else if(selection.equals(toyota))
-                {
-                    toyotaModels();
-                }
-                else if(selection.equals(chevrolet))
-                {
-                    chevroletModels();
-                }
+                String[] chevroletModel = {
+                        "Trailblazer",
+                        "Colorado",
+                        "Equinox",
+                        "Malibu",
+                        "Silverado LD",
+                        "Silverado HD",
+                        "Tahoe",
+                        "Trax",
+                        "Traverse"
+                };
+                chevroletList = new JComboBox(chevroletModel);
+                panel.add(chevroletList);
+                textboxForMileage();
+            }
+
+        }
+        panel.revalidate();
+        frame.pack();
+    }
+
+    public void selectionDeletion() {
+
+        if (fordList != null) {
+            panel.remove(fordList);
+            fordList = null;
+        }
+        if (hondaList != null) {
+            panel.remove(hondaList);
+            hondaList = null;
+        }
+        if (hyundaiList != null) {
+            panel.remove(hyundaiList);
+            hyundaiList = null;
+        }
+        if (chevroletList != null) {
+            panel.remove(chevroletList);
+            chevroletList = null;
+        }
+        if (toyotaList != null) {
+            panel.remove(toyotaList);
+            toyotaList = null;
+        }
+        if (labelModel != null) {
+            panel.remove(labelModel);
+            labelModel = null;
+        }
+        if (labelMileage != null) {
+            panel.remove(labelMileage);
+            labelMileage = null;
+        }
+        if (mileage != null) {
+            panel.remove(mileage);
+            mileage = null;
+        }
+        if (enterButton != null) {
+            panel.remove(enterButton);
+            enterButton = null;
+        }
+        panel.revalidate();
+        frame.pack();
+    }
+
+    public void textboxForMileage() {
+        labelMileage = new JLabel("Vehicle Mileage");
+        labelMileage.setFont(new Font("Arial", Font.PLAIN, 15));
+        labelMileage.setHorizontalAlignment(2);
+        panel.add(labelMileage);
+
+        NumberFormat mileageFormat = NumberFormat.getIntegerInstance();
+        NumberFormatter mileageFormatter = new NumberFormatter(mileageFormat);
+        mileageFormatter.setAllowsInvalid(false);
+        mileage = new JFormattedTextField(mileageFormatter);
+        panel.add(mileage);
+
+        enterButton = new JButton("Enter for Recommended Maintenance");
+        enterButton.setVerticalTextPosition(AbstractButton.CENTER);
+        enterButton.setHorizontalTextPosition(AbstractButton.LEADING);
+        enterButton.setMnemonic(KeyEvent.VK_ENTER);
+        enterButton.setEnabled(false);
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        panel.add(enterButton);
+        mileage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
             }
         });
     }
 
-    public void textboxForMileage()
-{
-    JLabel labelMileage = new JLabel("Vehicle Mileage");
-    labelMileage.setFont(new Font("Arial", Font.PLAIN, 15));
-    labelMileage.setHorizontalAlignment(2);
-    panel.add(labelMileage);
-
-    NumberFormat mileageFormat = NumberFormat.getIntegerInstance();
-    NumberFormatter mileageFormatter = new NumberFormatter(mileageFormat);
-    mileageFormatter.setAllowsInvalid(false);
-    JFormattedTextField mileage = new JFormattedTextField(mileageFormatter);
-    panel.add(mileage);
-
-    JButton enterButton = new JButton("Enter for Recommended Maintenance");
-    enterButton.setVerticalTextPosition(AbstractButton.CENTER);
-    enterButton.setHorizontalTextPosition(AbstractButton.LEADING);
-    enterButton.setMnemonic(KeyEvent.VK_ENTER);
-    enterButton.setEnabled(false);
-    enterButton.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    });
-    panel.add(enterButton);
-    mileage.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    });
-}
-public void modelLabel()
-{
-    JLabel labelModel = new JLabel("Model");
-    labelModel.setFont(new Font("Arial", Font.PLAIN, 15));
-    labelModel.setHorizontalAlignment(2);
-    panel.add(labelModel);
-    selectMake = new JLabel("Select Make");
-    selectMake.setFont(new Font("Arial", Font.PLAIN, 15));
-    panel.add(selectMake);
-    selectMake.isOpaque();
-}
-public void fordModels()
-{
-    String[] fordModel = {
-            "Explorer",
-            "Edge",
-            "Maverick",
-            "Transit",
-            "F-Series",
-         "Bronco",
-         "Mustang",
-         "Expedition",
-         "Bronco Sport"
-    };
-    JComboBox fordList = new JComboBox(fordModel);
-    panel.add(fordList);
-}
-public void toyotaModels()
-{
-    String[] toyotaModel = {
-         "Camry",
-         "Corolla",
-         "RAV4",
-         "Highlander",
-         "Tacoma",
-         "Tundra",
-         "Sienna",
-         "4Runner",
-         "Prius"
-    };
-    JComboBox toyotaList = new JComboBox(toyotaModel);
-    panel.add(toyotaList);
-}
-public void hyundaiModels()
-{
-    String[] hyundaiModel = {
-         "Tucson",
-         "Elantra",
-         "Palisade",
-         "Santa Fe",
-         "Kona",
-         "Ioniq-5",
-         "Sonata",
-         "Santa Cruz",
-         "Venue"
-    };
-    JComboBox hyundaiList = new JComboBox(hyundaiModel);
-    panel.add(hyundaiList);
-}
-public void hondaModels()
-{
-    String[] hondaModel = {
-            "CR-V",
-            "Civic",
-            "Accord",
-            "Pilot",
-            "HR-V",
-            "Odyssey",
-            "Prologue",
-            "Ridgeline",
-            "Passport"
-    };
-    JComboBox hondaList = new JComboBox(hondaModel);
-        panel.add(hondaList);
-}
-
-public void chevroletModels()
-{
-    String[] chevroletModel = {
-         "Trailblazer",
-         "Colorado",
-         "Equinox",
-         "Malibu",
-         "Silverado LD",
-         "Silverado HD",
-         "Tahoe",
-         "Trax",
-         "Traverse"
-    };
-    JComboBox chevroletList = new JComboBox(chevroletModel);
-        panel.add(chevroletList);
-
-}
+    public void modelLabel() {
+        labelModel = new JLabel("Model");
+        labelModel.setFont(new Font("Arial", Font.PLAIN, 15));
+        labelModel.setHorizontalAlignment(2);
+        panel.add(labelModel);
+    }
 
 
+    public static void main(String[] args)
+    {
+        new GUI();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 public class Main {
 
-    public static void main(String[] args)
-    {
-        new GUI();
-    }
+
     
 }
